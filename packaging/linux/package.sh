c73fe6a891702bb25ec8d802d2f48b7a2935221d
Alex <hash_browns@icloud.com>
2026-08-14 15:09:58 +0800

    fix: name portable binaries with os/arch and drop nfpm config from release assets

--- packaging/linux/package.sh
+++ packaging/linux/package.sh
@@ -11,13 +11,16 @@ OUT="$4"
 
 mkdir -p "$OUT"
 
+CFG="$(mktemp)"
+trap 'rm -f "$CFG"' EXIT
+
 sed -e "s/__ARCH__/${ARCH}/g" \
     -e "s/__VERSION__/${VERSION}/g" \
     -e "s#__BINARY__#${BINARY}#g" \
-    packaging/linux/nfpm.yaml > "$OUT/nfpm-${ARCH}.yaml"
+    packaging/linux/nfpm.yaml > "$CFG"
 
-nfpm package --config "$OUT/nfpm-${ARCH}.yaml" --packager deb --target "$OUT/talk-mirror_${VERSION}_${ARCH}.deb"
-nfpm package --config "$OUT/nfpm-${ARCH}.yaml" --packager rpm --target "$OUT/talk-mirror-${VERSION}-${ARCH}.rpm"
+nfpm package --config "$CFG" --packager deb --target "$OUT/talk-mirror_${VERSION}_${ARCH}.deb"
+nfpm package --config "$CFG" --packager rpm --target "$OUT/talk-mirror-${VERSION}-${ARCH}.rpm"
 
 echo "built:"
 ls -la "$OUT"
